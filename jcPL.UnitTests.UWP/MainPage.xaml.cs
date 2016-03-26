f03725e6e17028eb74b4f35e3b9314381deaf14d
Jarred Capellman <jcapellman@hotmail.com>
2016-03-26 01:26:24 -0400

    Impemented Unit Test for UWP

--- jcPL.UnitTests.UWP/MainPage.xaml.cs
+++ jcPL.UnitTests.UWP/MainPage.xaml.cs
@@ -1,30 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using jcPL.UnitTests.UWP.ViewModels;
 
-namespace jcPL.UnitTests.UWP
-{
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
+namespace jcPL.UnitTests.UWP {
+    public sealed partial class MainPage : Page {
+        private MainModel viewModel => (MainModel)DataContext;
+
+        public MainPage() {
             this.InitializeComponent();
+
+            DataContext = new MainModel();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e) {
+            var result = await viewModel.LoadData();
+        }
+
+        private async void ButtonBase_OnClick(object sender, RoutedEventArgs e) {
+            var result = await viewModel.LoadData();
         }
     }
 }
---
+++ jcPL.UnitTests.UWP/ViewModels/MainModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+using jcPL.PCL.Implementations;
+using jcPL.UWP.Lib;
+
+namespace jcPL.UnitTests.UWP.ViewModels {
+    public class MainModel : INotifyPropertyChanged {
+        private string _cacheString;
+
+        public string CacheString {
+            get { return _cacheString; }
+            set { _cacheString = value; OnPropertyChanged(); }
+        }
+
+        public async Task<bool> LoadData() {
+            var cacheHandler = new JSONPL(new UWPPS());
+
+            var item = await cacheHandler.GetAsync<string>("Test");
+
+            if (!item.HasValue) {
+                await cacheHandler.PutAsync("Test", DateTime.Now.ToString());
+
+                CacheString = DateTime.Now.ToString();
+            } else {
+                CacheString = item.Value;
+            }
+
+            return true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
